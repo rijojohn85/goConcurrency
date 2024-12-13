62c12aecf7eec879495a9759ab69c2c0946f9955
rijojohn85 <rijojohn85@gmail.com>
2024-12-13 15:52:23 +0530

    working on it

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = ":8080"
 
 func main() {
 	// connect to the database
-	initDB()
-	// create session
+	db := initDB()
+	defer db.Close()
+	// create session - connect to redis
+	session := initSession()
+	// create loggers
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR:\t", log.Ldate|log.Ltime)
 	// create channels
 	// createWaitGroup
+	wg := sync.WaitGroup{}
 	// set up the application config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		Wait:     &wg,
+		ErrorLog: errorLog,
+		InfoLog:  infoLog,
+	}
+
 	// set up mail
 	// listen for web connections
 }
@@ -31,10 +55,19 @@ func connectToDB() *sql.DB {
 	counts := 0
 	dsn := os.Getenv("DSN")
 	for {
+		if counts == 3 {
+			return nil
+		}
 		connection, err := openDB(dsn)
 		if err != nil {
 			log.Println("Postgres not yet ready...")
+			counts++
+		} else {
+			log.Println("DB Connected")
+			return connection
 		}
+		log.Println("Backing off for one second and retrying connection to db")
+		time.Sleep(time.Second)
 	}
 }
 
@@ -48,4 +81,26 @@ func openDB(dsn string) (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	// set up session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPood := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPood
 }
